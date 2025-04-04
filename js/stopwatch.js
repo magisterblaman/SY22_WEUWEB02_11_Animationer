d8ef4fdddf7dec4f933603f16586afe77ead102a
magisterblaman <hannes.blaman@lbs.se>
2025-04-04 10:22:39 +0200

    Stopwatch

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -11,7 +11,58 @@ let minutes = 0;
 let seconds = 0;
 
 startButton.addEventListener('click', startStopwatch);
+pauseButton.addEventListener('click', pauseStopwatch);
+resetButton.addEventListener('click', resetStopwatch);
+
+let stopwatchIntervalId = -1;
 
 function startStopwatch(event) {
-	
+	if (stopwatchIntervalId === -1) {
+		stopwatchIntervalId = setInterval(secondPassed, 50);
+	}
+}
+
+function pauseStopwatch(event) {
+	clearInterval(stopwatchIntervalId);
+	stopwatchIntervalId = -1;
+}
+
+function resetStopwatch(event) {
+	hours = 0;
+	minutes = 0;
+	seconds = 0;
+
+	updateElements();
+}
+
+function secondPassed() {
+	seconds++;
+	if (seconds >= 60) {
+		minutes++;
+		seconds = 0;
+	}
+	if (minutes >= 60) {
+		hours++;
+		minutes = 0;
+	}
+
+	updateElements();
+}
+
+function updateElements() {
+	if (hours < 10) {
+		hoursElement.innerText = '0' + hours;
+	} else {
+		hoursElement.innerText = hours;
+	}
+	if (minutes < 10) {
+		minutesElement.innerText = '0' + minutes;
+	} else {
+		minutesElement.innerText = minutes;
+	}
+	if (seconds < 10) {
+		secondsElement.innerText = '0' + seconds;
+	} else {
+		secondsElement.innerText = seconds;
+	}
 }
